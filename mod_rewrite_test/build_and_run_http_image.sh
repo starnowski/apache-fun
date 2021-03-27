567cd6a5b2f30238717f1f04cf6930b8ced51f42
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-27 18:21:37 +0100

    Added delay to check if docker container is ready for tests.

--- mod_rewrite_test/build_and_run_http_image.sh
+++ mod_rewrite_test/build_and_run_http_image.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+function waitUntilDockerContainerIsReady {
+    checkCount=1
+    timeoutInSeconds=20
+    while : ; do
+        RESULT="1"
+        if [ $(curl -LI http://localhost:8090 -o /dev/null -w '%{http_code}\n' -s) == "200" ];
+        then
+            RESULT="0"
+        fi
+        [[ "$RESULT" -ne 0 && $checkCount -ne $timeoutInSeconds ]] || break
+        checkCount=$(( checkCount+1 ))
+        echo "Waiting $checkCount seconds for apache to start"
+        sleep 1
+    done
+}
+
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=my-apache2 --format="{{.ID}}"))
 
 ## Replace host with correct url
@@ -15,6 +31,8 @@ echo " Server properties: ip: ${ANOTHER_SERVER_IP}, port: ${ANOTHER_SERVER_PORT}
 
 docker build -t my-apache2 .
 docker run -dit --name my-running-app -p 8090:80 my-apache2
+
+waitUntilDockerContainerIsReady
 
 tmpfile=$(mktemp)
 curl localhost:8090/show/my/staff/domains > "$tmpfile"
