3f90aedcedb49bedf6e81d9fa0cc6ba0285bd3db
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-27 13:57:25 +0100

    Adjustments in tests script.

--- mod_rewrite_test/build_and_run_http_image.sh
+++ mod_rewrite_test/build_and_run_http_image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=my-apache2 --format="{{.ID}}"))
 docker build -t my-apache2 .
 docker run -dit --name my-running-app -p 8090:80 my-apache2
 #docker run -it --name my-running-app -p 8090:80 my-apache2
-#docker rm $(docker stop $(docker ps -a -q --filter name=my-apache2 --format="{{.ID}}"))
 
 curl localhost:8090/show/my/staff/domains
-docker rm $(docker ps -a -q --filter ancestor=my-apache2 --format="{{.ID}}")
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=my-apache2 --format="{{.ID}}"))
